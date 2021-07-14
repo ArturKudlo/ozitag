258ec39dbb7015ba1beedea00247721d47cfe833
arturkudli <arturkapes@gmail.com>
2021-07-14 18:30:58 +0300

    add client validation

--- src/components/pages/auth/LoginForm/LoginForm.tsx
+++ src/components/pages/auth/LoginForm/LoginForm.tsx
@@ -6,6 +6,24 @@ type MyFormValues = {
     password: string
 }
 
+const validateEmail= (value: string): string | undefined =>  {
+    let error;
+    if (!value) {
+        error = 'Email required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+        error = 'Invalid email address';
+    }
+    return error;
+}
+
+const validatePassword = (value: string): string | undefined => {
+    let error;
+    if (value.length < 6) {
+        error = 'Minimum 6 symbols required';
+    }
+    return error;
+}
+
 export const LoginForm = () => {
     const initialValues: MyFormValues = {email: '', password: ''}
     return (
@@ -13,15 +31,18 @@ export const LoginForm = () => {
             <Formik
                 initialValues={initialValues}
                 onSubmit={async (values) => {
-                    await new Promise((resolve) => setTimeout(resolve, 500));
-                    alert(JSON.stringify(values, null, 2));
+                    console.log({values})
                 }}
             >
-                <Form>
-                    <Field name="email" type="email" />
-                    <Field name="password" type="password" />
-                    <button type="submit">Submit</button>
-                </Form>
+                {({ errors, touched, isValidating }) => (
+                    <Form>
+                        <Field name="email" type="email" validate={validateEmail}/>
+                        {errors.email && touched.email && <div>{errors.email}</div>}
+                        <Field name="password" type="password" validate={validatePassword}/>
+                        {errors.password && touched.password && <div>{errors.password}</div>}
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
             </Formik>
         </div>
     )
