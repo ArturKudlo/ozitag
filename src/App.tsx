77dbf3744316633668e9159d72aa12da46c5df98
arturkudli <arturkapes@gmail.com>
2021-07-14 17:34:15 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import {PageAuth, PageProfile} from "./components/pages";
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-        <PageAuth/>
-        <PageProfile/>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <PageProfile />
+                </Route>
+                <Route exact path="/login">
+                    <PageAuth />
+                </Route>
+                <Route path="*">
+                    <Redirect to="/" />
+                </Route>
+            </Switch>
+        </Router>
     </div>
   );
 }
