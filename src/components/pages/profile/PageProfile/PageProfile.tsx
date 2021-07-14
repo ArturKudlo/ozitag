8fee10f04559e77c94942bc9b64040253ff63930
arturkudli <arturkapes@gmail.com>
2021-07-14 20:10:09 +0300

    add localstorage for profile

--- src/components/pages/profile/PageProfile/PageProfile.tsx
+++ src/components/pages/profile/PageProfile/PageProfile.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useHistory } from "react-router-dom";
 
 export const PageProfile = () => {
+    const history = useHistory();
+    useEffect(() => {
+        const loginData = localStorage.getItem('loginData')
+        if (!loginData) {
+            history.push("/login");
+        }
+    },[])
+
     return (
         <div>
             <h2>Page Profile</h2>
